afc4ef71d0b15ed47adb8735cfe7157a2c83355a
Lam Nguyen <lnguye16@gustavus.edu>
2018-06-12 08:16:01 -0500

    edited

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const {Client} = require('pg');
 const postgres = knex({
   client: 'pg',
   connection: {
-    connectionString : process.env.DATABASE_URI,
+    connectionString : process.env.DATABASE_URL,
   	ssl: true,
   }
 });
